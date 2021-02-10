4c3a86e05772cef928028063be93d849cb896616
dekadentno <matej.lazarevich@gmail.com>
2021-02-10 23:39:53 +0100

    CSS structure

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="body__inner" style="background: pink">
+    <div class="body__inner--bla">aaa</div>
     <TopBar />
     <transition name="fade" appear>
-      <div>aaa222aaaa</div>
+      <div>aaa222aaaaaaa222aaaaaaa222aaaaaaa222aaaaaaa222aaaaaaa222aaaaaaa222aaaaaaa222aaaaaaa222aaaaaaa222aaaa</div>
     </transition>
   </div>
 </template>
