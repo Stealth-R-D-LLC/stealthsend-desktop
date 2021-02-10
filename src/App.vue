276398394d760d2fa6fd06af86a1aef64c0aa122
Matej Lazrevic <matej.lazarevic@barrage.net>
2021-02-10 14:01:31 +0100

    Init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="body__inner" style="background: pink">
+    <TopBar />
+    <!-- [ HERE COMES YOUR CONTENT ] -->
+    <transition name="fade" appear>
+      <div>aaaaaaa</div>
+    </transition>
+  </div>
+</template>
+
+<script>
+import TopBar from '/~/components/layout/TopBar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TopBar,
+  },
+}
+</script>
