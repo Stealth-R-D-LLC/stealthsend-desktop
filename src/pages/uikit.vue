43bb3ed383be59290ad6011bf2fac5440998a3ba
Matej Lazrevic <matej.lazarevic@barrage.net>
2021-02-11 13:29:46 +0100

    XSTDESKTOP-1 improve layout and sidebar menu

---
+++ src/pages/uikit.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="uikit-container">
+    ui kit
+    <StButton @click="clickHandler('aaaaa')">Dash</StButton>
+    <StButton @click="clickHandler('bbbbb')">Settings</StButton>
+    <StInput></StInput>
+  </div>
+</template>
+
+<script>
+import StButton from '/~/components/kit/StButton.vue'
+import StInput from '/~/components/kit/StInput.vue'
+export default {
+      components: {
+    StButton,
+    StInput,
+  },
+  setup() {
+    function clickHandler(test) {
+      console.log('---> ', test)
+    }
+    return {
+      clickHandler,
+    }
+  },
+}
+</script>
+
+<style scoped></style>
