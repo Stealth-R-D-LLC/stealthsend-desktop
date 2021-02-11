29468f7126025fca1cec7ac6d42e3f8b4fcd6688
Matej Lazrevic <matej.lazarevic@barrage.net>
2021-02-11 13:15:20 +0100

    XSTDESKTOP-1 implement router

---
+++ src/components/layout/Default.vue
@@ -0,0 +1,16 @@
+<template>
+    <router-view></router-view>
+</template>
+
+<script>
+export default {
+    name: 'TsDefault',
+    setup () {
+        return {}
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/dashboard.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="dashbaord-container">dashboard</div>
+</template>
+
+<script>
+export default {
+  name: 'StDshboard',
+  setup() {
+    return {}
+  },
+}
+</script>
+
+<style scoped></style>
