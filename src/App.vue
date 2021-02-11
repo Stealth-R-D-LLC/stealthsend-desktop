af230064120bd8c76b328828cfb9590a18e9b30b
dekadentno <matej.lazarevich@gmail.com>
2021-02-11 11:35:33 +0100

    XSTDESKTOP-1 input component

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,36 @@
 <template>
   <div class="default-layout">
-      <aside class="default-layout__aside">lijesvo</aside>
+    <aside class="default-layout__aside">lijesvo</aside>
     <main class="default-layout__main">
-    <header class="default-layout__header">headeraaa</header>
-        <p>aaa</p>
-        <p>aaa</p>
-        <p>aaa</p>
-        <p>aaa</p>
-        <p>aaa</p>
-        <p>aaa</p>
-        <p>aaa</p>
-        <p>aaa</p>
-        <p>aaa</p>
-        <p>aaa</p>
-        <p>aaa</p>
-        <p>aaa</p>
-        <p>aaa</p>
-        <StButton @click="clickHandler">Button</StButton>
+      <header class="default-layout__header">headeraaa</header>
+      <p>aaa</p>
+      <p>aaa</p>
+      <p>aaa</p>
+      <p>aaa</p>
+      <p>aaa</p>
+      <p>aaa</p>
+      <p>aaa</p>
+      <p>aaa</p>
+      <p>aaa</p>
+      <p>aaa</p>
+      <p>aaa</p>
+      <p>aaa</p>
+      <p>aaa</p>
+      <StButton @click="clickHandler">Button</StButton>
+      <StInput></StInput>
     </main>
   </div>
 </template>
 
 <script>
 import StButton from '/~/components/kit/StButton.vue'
+import StInput from '/~/components/kit/StInput.vue'
 
 export default {
   name: 'App',
   components: {
     StButton,
+    StInput,
   },
   setup() {
     function clickHandler() {
---
+++ src/components/kit/StInput.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="st-input" :class="{ 'st-input--has-icon': icon }">
+    <TsIcon v-if="icon" :name="icon" />
+    <input
+      ref="input"
+      :type="type"
+      :disabled="disabled"
+      :placeholder="placeholder"
+      autocomplete="off"
+      class="st-input__inner"
+      :class="{ 'is-disabled': disabled }"
+      :value="value"
+      @input="$emit('input', $event.target.value)"
+      @change="$emit('change', $event.target.value)"
+      @keyup="$emit('keyup', $event.target.value)"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'StInput',
+  props: {
+    icon: {
+      type: String,
+      required: false,
+      default: () => {
+        return ''
+      },
+    },
+    value: {
+      type: String,
+      required: false,
+      default: () => {
+        return ''
+      },
+    },
+    disabled: {
+      type: Boolean,
+      required: false,
+      default: () => {
+        return false
+      },
+    },
+    type: {
+      type: String,
+      required: false,
+      default: () => {
+        return 'text'
+      },
+    },
+    placeholder: {
+      type: String,
+      required: false,
+      default: () => {
+        return ''
+      },
+    },
+  },
+  emits: ['change', 'input', 'keyup'],
+}
+</script>
+
+<style scoped>
+
+</style>
