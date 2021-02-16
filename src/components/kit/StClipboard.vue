3b0a9891bc1a988d4390b545a5e3e7dd0d5c28a0
Matej Lazrevic <matej.lazarevic@barrage.net>
2021-02-16 15:08:30 +0100

    XSTDESKTOP-1 tooltip component

---
+++ src/components/kit/StClipboard.vue
@@ -0,0 +1,34 @@
+<template>
+  <span class="st-clipboard" @click="copy">Copy to clipboard (click me)</span>
+</template>
+
+<script>
+export default {
+  name: 'StCopyToClipboard',
+  props: {
+    content: {
+      type: String,
+      required: true,
+    },
+  },
+  setup(props) {
+    function copy() {
+      navigator.clipboard.writeText(props.content).then(
+        () => {},
+        function (err) {
+          console.warn('Async: Could not copy to clipboard: ', err)
+        }
+      )
+    }
+    return { copy }
+  },
+}
+</script>
+
+<style lang="postcss" scoped>
+.st-clipboard {
+  &:hover {
+    cursor: pointer;
+  }
+}
+</style>
