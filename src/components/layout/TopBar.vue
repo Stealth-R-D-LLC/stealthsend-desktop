a6e06697ac5ce80431207569ee9dc921814bf451
Matej Lazrevic <matej.lazarevic@barrage.net>
2021-02-17 11:09:24 +0100

    XSTDESKTOP-1 render package.json version in header

--- src/components/layout/TopBar.vue
+++ src/components/layout/TopBar.vue
@@ -1,5 +1,15 @@
 <template>
   <header class="default-layout__header">
-    Welcome, Matej
+    Welcome, Matej {{version}}
   </header>
 </template>
+
+<script>
+import pkgjson from '../../../package.json'
+export default {
+  setup() {
+    let version = pkgjson.version
+    return { version }
+  }
+}
+</script>
