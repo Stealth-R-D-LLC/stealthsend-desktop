22dcd120132fd9fa00eea18d369596ff32030320
dekadentno <matej.lazarevich@gmail.com>
2021-02-17 11:05:28 +0100

    XSTDESKTOP-1 implement tooltip component and style

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
-import { createApp } from 'vue'
+import { createApp } from 'vue';
+import directives from "./directives/";
 // import VueClickAway from 'vue3-click-away'
-import router from './router'
-import App from '/~/App.vue'
-import '/~/index.css'
+import router from './router';
+import App from '/~/App.vue';
+import '/~/index.css';
 
 const app = createApp(App)
+
+// register all directives
+directives(app)
 
 app.use(router)
 app.mount('#app')
