13bc1aafffa880f6ca95e2f663feaa02460ce0f9
dekadentno <matej.lazarevich@gmail.com>
2021-02-18 13:29:00 +0100

    XSTDESKTOP-1 implement and configure axios

--- src/main.js
+++ src/main.js
@@ -1,11 +1,25 @@
+import Multiselect from '@vueform/multiselect';
 import { createApp } from 'vue';
 import directives from "./directives/";
-// import VueClickAway from 'vue3-click-away'
 import router from './router';
 import App from '/~/App.vue';
+import StButton from '/~/components/kit/StButton.vue';
+import StCheckbox from '/~/components/kit/StCheckbox.vue';
+import StInput from '/~/components/kit/StInput.vue';
+import StLink from '/~/components/kit/StLink.vue';
+import StRadio from '/~/components/kit/StRadio.vue';
 import '/~/index.css';
 
 const app = createApp(App)
+
+// global components
+app.component('StButton', StButton)
+app.component('StInput', StInput)
+app.component('StMultiselect', Multiselect)
+app.component('StLink', StLink)
+app.component('StRadio', StRadio)
+app.component('StCheckbox', StCheckbox)
+// end global components
 
 // register all directives
 directives(app)
