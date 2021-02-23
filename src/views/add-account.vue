9766c7692a27bb4aefd0adf2b8dc621652e3cad6
Matej Lazrevic <matej.lazarevic@barrage.net>
2021-02-23 13:18:08 +0100

    XSTDESKTOP-1 store and read pk and pass from db

--- src/views/add-account.vue
+++ src/views/add-account.vue
@@ -1,31 +1,39 @@
 <template>
-    <div class="add-account-container">
-        Add account
-    </div>
+  <div class="add-account-container">
+    Add account
+    <button @click="generate">generate</button>
+    <button @click="storePKinDB">store pk in db</button>
+    <button @click="getPKfromDB">getPKfromDB</button>
+  </div>
 </template>
 
 <script>
-import { onMounted } from 'vue'
-import CryptoService from '../services/crypto';
+import CryptoService from '../services/crypto'
 
 export default {
-    name: 'StAddAccount',
-    setup () {
-
-        onMounted(() => {
-            CryptoService.generateMnemonicAndSeed()
-            console.log('mnemonic: ', CryptoService.mnemonic);
-            console.log('is mnemonic valid: ', CryptoService.isMnemonicValid());
-            console.log('random address: ', CryptoService.generateRadnomAddress());
-
-        })
-        return {
-
-        }
+  name: 'StAddAccount',
+  setup() {
+    function storePKinDB() {
+      CryptoService.storePKinDB()
     }
+    function getPKfromDB() {
+      CryptoService.getPk().then((res) => {
+        console.log('db: ', res)
+      })
+    }
+    function generate() {
+      CryptoService.generateMnemonicAndSeed()
+      console.log('mnemonic: ', CryptoService.mnemonic)
+      console.log('is mnemonic valid: ', CryptoService.isMnemonicValid())
+      console.log('random address: ', CryptoService.generateRandomAddress())
+    }
+    return {
+      storePKinDB,
+      getPKfromDB,
+      generate
+    }
+  }
 }
 </script>
 
-<style lang="postcss" scoped>
-
-</style>
+<style lang="postcss" scoped></style>
