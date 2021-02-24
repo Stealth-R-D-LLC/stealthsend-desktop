b9ca2299b418276b04da77ca956151efa650d2b9
Matej Lazrevic <matej.lazarevic@barrage.net>
2021-02-24 15:01:55 +0100

    XSTDESKTOP-1 generate entropy with mouse movements

--- src/views/add-account.vue
+++ src/views/add-account.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="add-account-container">
     Add account
+    <button @click="henerateEntropyWithMouse">henerateEntropyWithMouse</button>
     <button @click="generateMnemonic">generate mnemonic</button>
     <button @click="generateRandomAddress">generate random address</button>
     <button @click="storeWalletInDb">storeWalletInDb</button>
@@ -15,7 +16,55 @@ import CryptoService from '../services/crypto'
 export default {
   name: 'StAddAccount',
   setup() {
-    
+    function henerateEntropyWithMouse() {
+      const entropy = []
+      let captureStart = false
+
+      document.addEventListener('mousemove', function(e) {
+        const MAX_LEN = 16 // size of entropy's array
+        if (entropy.length >= MAX_LEN) return
+        const now = Date.now()
+        if (now >= 1 && now % 10 !== 0) return
+        if (!captureStart) {
+          return setTimeout(() => {
+            captureStart = true
+          }, 3000) // capturing starts in 3 seconds to set the mouse cursor at random position...
+        }
+        const iw = window.innerWidth
+        const ih = window.innerHeight
+        const iwPlusIh = iw + ih
+        const px = e.pageX
+        const py = e.pageY
+        const pxPlusPy = px + py
+        const ret = Math.round((pxPlusPy / iwPlusIh) * 255)
+        entropy.push(ret)
+        // console.log('0-255:', ret)
+        if (entropy.length >= MAX_LEN) {
+        //   console.log('entropy:', entropy)
+          shuffle(entropy)
+          console.log('suffledEntropy:', entropy)
+          //     const account = WalletUtil.generateAccount({ entropy });
+          //     console.log("account:", JSON.stringify(account, null, 2));
+        }
+
+        function shuffle(array) {
+          let currentIndex = array.length,
+            temporaryValue,
+            randomIndex
+          // While there remain elements to shuffle...
+          while (0 !== currentIndex) {
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex)
+            currentIndex -= 1
+            // And swap it with the current element.
+            temporaryValue = array[currentIndex]
+            array[currentIndex] = array[randomIndex]
+            array[randomIndex] = temporaryValue
+          }
+          return array
+        }
+      })
+    }
     function storeWalletInDb() {
       CryptoService.storeWalletInDb()
     }
@@ -37,6 +86,7 @@ export default {
       console.log('random address: ', CryptoService.generateRandomAddress())
     }
     return {
+      henerateEntropyWithMouse,
       storeWalletInDb,
       getWalletFromDb,
       generateMnemonic,
