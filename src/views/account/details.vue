b3d1eef70d546ac26bb4751e670729f070e34457
dekadentno <matej.lazarevich@gmail.com>
2021-03-10 12:21:03 +0100

    XSTDESKTOP-1 list address trxs

--- src/views/account/details.vue
+++ src/views/account/details.vue
@@ -1,34 +1,60 @@
 <template>
   <div class="account-details-container">
-      <h1>Account details</h1>
+    <h1>Account details</h1>
     <pre>
           {{ account }}
-          {{addressInfo}}
+          {{ addressInfo }}
       </pre
     >
+    <StTable
+      :data="trxOutputs"
+      :columns="[
+        { key: 'txid', title: 'TRX ID' },
+        { key: 'amount', title: 'Amount' },
+        { key: 'confirmations', title: 'Confirmations' },
+        { key: 'balance', title: 'Balance' }
+      ]"
+    ></StTable>
   </div>
 </template>
 
 <script>
 import globalState from '@/store/global'
+import StTable from '@/components/kit/StTable.vue'
 import { computed, ref } from 'vue'
 
 export default {
   name: 'StAccountDetails',
+  components: {
+    StTable
+  },
   setup() {
     const account = computed(() => {
       return globalState.state.accountDetails
     })
 
     const addressInfo = ref({})
-    globalState.rpc('getaddressinfo', ['mtZNzEATaAyQinTEGu3Ye7EAM5fk75R4b7']).then(res => {
-      addressInfo.value = res
-    }).catch(err => {
-      return err
-    })
+    const trxOutputs = ref({})
+    globalState
+      .rpc('getaddressinfo', ['mtZNzEATaAyQinTEGu3Ye7EAM5fk75R4b7'])
+      .then((res) => {
+        addressInfo.value = res
+      })
+      .catch((err) => {
+        return err
+      })
+    globalState
+      .rpc('getaddressoutputs', ['mtZNzEATaAyQinTEGu3Ye7EAM5fk75R4b7'])
+      .then((res) => {
+        trxOutputs.value = res
+      })
+      .catch((err) => {
+        return err
+      })
     return {
       account,
-      addressInfo
+      addressInfo,
+      trxOutputs
     }
   }
 }
