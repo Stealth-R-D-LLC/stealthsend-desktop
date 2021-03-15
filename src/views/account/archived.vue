e84de9cbc6b0e027de319ddf6fbf41664312a45e
Matej Lazrevic <matej.lazarevic@barrage.net>
2021-03-15 13:08:46 +0100

    XSTDESKTOP-1 unarchive account

--- src/views/account/archived.vue
+++ src/views/account/archived.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="archived-accounts-container">
         <transition-group v-if="accounts.length !== 0" name="list" tag="div">
-      <card v-for="account in accounts" :key="account" class="list-item" :account="account">
+      <card v-for="account in accounts" :key="account" class="list-item" :account="account" @unarchived="unarchieve">
       </card>
     </transition-group>
   </div>
@@ -11,6 +11,7 @@
 import globalState from '@/store/global'
 import Card from '@/components/elements/Card'
 import { computed } from 'vue'
+import CryptoService from '@/services/crypto'
 
 export default {
   name: 'StArchivedAccounts',
@@ -21,8 +22,13 @@ export default {
     const accounts = computed(() => {
       return globalState.state.accounts.filter((el) => el.isArchived)
     })
+    function unarchieve(account) {
+      console.log('aa');
+      CryptoService.unarchiveAccount(account)
+    }
     return {
-      accounts
+      accounts,
+      unarchieve
     }
   }
 }
