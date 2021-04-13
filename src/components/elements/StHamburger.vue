a683f4325f622489f331fa872f1a11cb44912a5b
abajivic <antonio.bajivic@barrage.net>
2021-04-13 13:08:04 +0200

    Last conflicts resolved

--- src/components/elements/StHamburger.vue
+++ src/components/elements/StHamburger.vue
@@ -1,16 +1,6 @@
 <template>
-<<<<<<< HEAD
   <div class="arrow" :class="{ 'is-open': open, 'is-closed': !open }">
       <div class="arrow-container"></div>
-=======
-  <div class="hamburglar" :class="{ 'is-open': open, 'is-closed': !open }">
-    <div class="burger-icon">
-      <div class="burger-container">
-        <span class="burger-bun-top"></span>
-        <span class="burger-bun-bot"></span>
-      </div>
-    </div>
->>>>>>> develop
   </div>
 </template>
 
@@ -21,11 +11,7 @@ export default {
     open: {
       type: Boolean,
       required: true,
-<<<<<<< HEAD
       default: true,
-=======
-      default: false,
->>>>>>> develop
     },
   },
   setup() {
@@ -64,52 +50,11 @@ export default {
   display: block;
   width: 20px;
   height: 2px;
-<<<<<<< HEAD
   border-bottom: 2px solid #fcfaff;
   transform: rotate(-45deg);
   position: absolute;
   top: 3px;
   left: -5px;
-=======
-  width: 27px;
-  background: #fcfaff;
-}
-
-.burger-bun-top {
-  top: 0;
-  transform-origin: 25.5px 3.5px;
-}
-
-.burger-bun-bot {
-  bottom: 0;
-  transform-origin: 20.5px -0.5px;
-}
-
-.hamburglar.is-open .burger-bun-top {
-  -webkit-animation: bun-top-out 0.6s linear normal;
-  animation: bun-top-out 0.6s linear normal;
-  -webkit-animation-fill-mode: forwards;
-  animation-fill-mode: forwards;
-}
-.hamburglar.is-open .burger-bun-bot {
-  -webkit-animation: bun-bot-out 0.6s linear normal;
-  animation: bun-bot-out 0.6s linear normal;
-  -webkit-animation-fill-mode: forwards;
-  animation-fill-mode: forwards;
-}
-
-.hamburglar.is-closed .burger-bun-top {
-  -webkit-animation: bun-top-in 0.6s linear normal;
-  animation: bun-top-in 0.6s linear normal;
-  -webkit-animation-fill-mode: forwards;
-  animation-fill-mode: forwards;
-}
-.hamburglar.is-closed .burger-bun-bot {
-  -webkit-animation: bun-bot-in 0.6s linear normal;
-  animation: bun-bot-in 0.6s linear normal;
-  -webkit-animation-fill-mode: forwards;
-  animation-fill-mode: forwards;
->>>>>>> develop
 }
 
 .arrow.is-open .arrow-container {
