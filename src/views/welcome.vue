f27c94ce8bc8b4e7af70d8f3dab25671e82797d1
jbjezancevic <josip.bjezancevic@barrage.net>
2021-05-14 13:56:32 +0200

    WIP XST-159 changes...

--- src/views/welcome.vue
+++ src/views/welcome.vue
@@ -75,6 +75,8 @@ import * as bip32 from 'bip32';
 import { useMainStore } from '@/store';
 import router from '../router';
 import CryptoService from '../services/crypto';
+import * as bitcoin from 'bitcoinjs-lib';
+
 
 export default {
   name: 'StWelcome',
@@ -103,32 +105,48 @@ export default {
 
       // Get the BIP32 root key in order to derive all addresses or make a lookup on the network
       console.log('-----------------------------------------------------');
-      console.log('bip32 root key 1)', master.toBase58()); // BIP32 Root Key (private key, check wallet generator)
+      console.log('1) BIP32 Root Key', master.toBase58()); 
       console.log('-----------------------------------------------------');
       console.log(
-        'bip32 root key 2)',
+        '2) Wallet Account Extended Private Key',
         master.derivePath("m/44'/1'/0'").toBase58()
-      ); // Account Extended Private Key (check wallet generator)
+      );
       console.log(
-        'bip32 root key 3)',
+        '3) Wallet Account Extended Public Key',
         master.derivePath("m/44'/1'/0'").neutered().toBase58()
-      ); // Account Extended Public Key (check wallet generator)
+      );
       console.log(
-        'bip32 root key 4)',
-        master.derivePath("m/44'/1'/0'/0").neutered().toBase58()
-      ); // First address BIP32 Extended Public Key (check wallet generator)
-      console.log('-----------------------------------------------------');
-      console.log(
-        'bip 32 root key 5)',
+        '4) BIP32 Extended Private Key',
         master.derivePath("m/44'/1'/0'/0").toBase58()
-      ); // BIP32 Extended Private Key (check wallet generator)
+      );
       console.log(
-        'bip 32 root key 6)',
+        '5) First account BIP32 Extended Public Key',
         master.derivePath("m/44'/1'/0'/0").neutered().toBase58()
-      ); // BIP32 Extended Public Key (check wallet generator)
-      // const bitcoin = require('bitcoinjs-lib')
-      // console.log("Object.keys(bitcoin)", Object.keys(bitcoin));
-      // console.log(bitcoin.bip32.fromSeed());
+      );
+      console.log('-----------------------------------------------------');
+      // WIF hoces?
+      console.log(
+        '6) First account WIF',
+        master.derivePath("m/44'/1'/0'/0").toWIF() // Do we need this for import or that under 7) ???
+      );
+      console.log(
+        '7) First account address WIF',
+        master.derivePath("m/44'/1'/0'/0/0").toWIF()
+      );
+      const accountWif = master.derivePath("m/44'/1'/0'/0").toWIF();
+      console.log('accountWif', accountWif);
+      console.log('Object.keys(bitcoin)', Object.keys(bitcoin));
+      console.log('Object.keys(bitcoin.address)', Object.keys(bitcoin.address));
+      console.log('Object.keys(bitcoin.payments)', Object.keys(bitcoin.payments));
+      console.log('Object.keys(bitcoin.ECPair)', Object.keys(bitcoin.ECPair));
+      
+      const accountFromWif = bitcoin.ECPair.fromWIF(accountWif, CryptoService.network);
+      console.log('accountFromWif', accountFromWif);
+      const accountFromWifPrivateKey = accountFromWif.privateKey;
+      const accountFromWifPublicKey = accountFromWif.publicKey;
+      
+      console.log('accountFromWifPrivateKey', accountFromWifPrivateKey.toString('hex'));
+      console.log('accountFromWifPublicKey', accountFromWifPublicKey.toString('hex'));
 
       const hardcodedPassword = '123456'; // hardcoded for now, missing wallet account step prior to importing the mnemonic
 
