a874e24554d62f4cb0ffdfb0f66f35cf0235505a
Matej Lazrevic <matej.lazarevic@barrage.net>
2021-05-26 08:24:06 +0200

    implement payment code component logic

--- src/components/elements/PaymentCode.vue
+++ src/components/elements/PaymentCode.vue
@@ -1,43 +1,15 @@
 <template>
   <div class="payment-code">
     <input
+      v-for="(digit, index) in digits"
       type="text"
-      class="digit-1"
+      :key="index"
       maxlength="1"
-      name="digit-1"
-      data-next="digit-2"
-    />
-    <input
-      type="text"
-      class="digit-2"
-      maxlength="1"
-      name="digit-2"
-      data-next="digit-3"
-      data-previous="digit-1"
-    />
-    <input
-      type="text"
-      class="digit-3"
-      maxlength="1"
-      name="digit-3"
-      data-next="digit-4"
-      data-previous="digit-2"
-    />
-    <input
-      type="text"
-      class="digit-4"
-      maxlength="1"
-      name="digit-4"
-      data-next="digit-5"
-      data-previous="digit-3"
-    />
-    <input
-      type="text"
-      class="digit-5"
-      maxlength="1"
-      name="digit-5"
-      data-next="digit-6"
-      data-previous="digit-4"
+      :class="`digit-${index}`"
+      :name="`digit-${index}`"
+      :data-next="index !== digits - 1 ? `digit-${index + 1}` : undefined"
+      :data-previous="index !== 0 ? `digit-${index - 1}` : undefined"
+      @keyup="handleKeyup"
     />
   </div>
 </template>
@@ -45,33 +17,51 @@
 <script>
 export default {
   name: 'StPaymentCode',
+  props: {
+    digits: {
+      type: Number,
+      required: false,
+      default: 5,
+    },
+  },
   setup() {
-    /*
-      $('.payment-code').find('input').each(function() {
-	$(this).attr('maxlength', 1);
-	$(this).on('keyup', function(e) {
-		console.log("ww", e)
-		var parent = $($(this).parent());
-		
-		// backspace or left arrow
-		if(e.keyCode === 8 || e.keyCode === 37) {
-			var prev = parent.find('input#' + $(this).data('previous'));
-			
-			if(prev.length) {
-				$(prev).select();
-			}
-			// 48-57 are 0-9, 96-105 0-9 on numpad, 39 is right arrow
-		} else if((e.keyCode >= 48 && e.keyCode <= 57) || (e.keyCode >= 96 && e.keyCode <= 105) || e.keyCode === 39) {
-			var next = parent.find('input.' + $(this).data('next'));
-			
-			if(next.length) {
-				$(next).select();
-			} 
-		}
-	});
-});
-      */
-    return {};
+    function handleKeyup(e) {
+      var parent = e.srcElement.parentNode;
+
+      // backspace or left arrow
+      if (e.keyCode === 8 || e.keyCode === 37) {
+        var prev = parent.querySelectorAll(
+          'input.' + e.srcElement.getAttribute('data-previous')
+        )[0];
+
+        if (prev) {
+          prev.focus();
+        }
+
+      // 48-57 are 0-9, 96-105 0-9 on numpad, 39 is right arrow
+      } else if (
+        (e.keyCode >= 48 && e.keyCode <= 57) ||
+        e.keyCode === 39
+      ) {
+        var next = parent.querySelectorAll(
+          'input.' + e.srcElement.getAttribute('data-next')
+        )[0];
+
+        if (next) {
+          next.focus();
+        }
+        e.target.value = e.key;
+      } else {
+          e.preventDefault();
+          e.target.value = '';
+          e.target.focus()
+          return false;
+      }
+    }
+
+    return {
+      handleKeyup,
+    };
   },
 };
 </script>
@@ -87,4 +77,5 @@ export default {
   font-weight: 200;
   margin: 0 2px;
 }
+
 </style>
