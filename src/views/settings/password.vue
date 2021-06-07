2c5347bf504b5de30e73546798b5cc53a6569672
dekadentno <matej.lazarevich@gmail.com>
2021-06-07 14:25:01 +0200

    XST-156 change password

--- src/views/settings/password.vue
+++ src/views/settings/password.vue
@@ -17,15 +17,33 @@
         lose my password, the only access to your account will be through the
         use of Recovery Phrase.
       </p>
-        <StFormItem label="Current password" notice="Please enter a password you are using to access the application">
-          <StInput type="password" placeholder="Please enter current password"></StInput>
-        </StFormItem>
-        <StFormItem label="New password" notice="Please enter a unique and unused password for accessing the application">
-          <StInput type="password" placeholder="Please enter new password"></StInput>
-        </StFormItem>
-        <StFormItem label="Confirm password" notice="Newly set password need to match">
-          <StInput type="password" placeholder="Please confirm new password"></StInput>
-        </StFormItem>
+      <StFormItem
+        label="Current password"
+        notice="Please enter a password you are using to access the application"
+      >
+        <StInput
+          type="password"
+          placeholder="Please enter current password"
+        ></StInput>
+      </StFormItem>
+      <StFormItem
+        label="New password"
+        notice="Please enter a unique and unused password for accessing the application"
+      >
+        <StInput
+          type="password"
+          placeholder="Please enter new password"
+        ></StInput>
+      </StFormItem>
+      <StFormItem
+        label="Confirm password"
+        notice="Newly set password need to match"
+      >
+        <StInput
+          type="password"
+          placeholder="Please confirm new password"
+        ></StInput>
+      </StFormItem>
     </div>
   </div>
 </template>
