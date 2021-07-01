68cf33a5a9945591499b040eaf83a50193608d63
dekadentno <matej.lazarevich@gmail.com>
2021-07-01 13:28:17 +0200

    XST-475

--- src/background.js
+++ src/background.js
@@ -13,8 +13,8 @@ protocol.registerSchemesAsPrivileged([
 async function createWindow() {
   // Create the browser window.
   const win = new BrowserWindow({
-    // width: 1152,
-    // height: 700,
+    width: 1152,
+    height: 700,
     minWidth: 1152,
     minHeight: 700,
     maxWidth: 1600,
