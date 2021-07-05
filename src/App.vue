e1e985ab0507dff55bcc91745a240a9f3f577b78
Alex Dragun <alex.dragun@barrage.net>
2021-07-05 16:17:56 +0200

    Eslint

--- src/App.vue
+++ src/App.vue
@@ -43,7 +43,7 @@ export default {
     });
     const isOnline = computed(() => {
       return online.value;
-    })
+    });
     const modals = computed(() => {
       return mainStore.modals;
     });
