3840883f251bf66dfa96673c159c3d7831364542
Matej Lazrevic <matej.lazarevic@barrage.net>
2021-07-06 21:03:45 +0200

    stop loader on validation error on set pass on recover

--- src/components/elements/Card.vue
+++ src/components/elements/Card.vue
@@ -300,10 +300,10 @@ export default {
 
 .card-header h6 {
   display: inline-block;
-    width: 260px;
-    white-space: nowrap;
-    overflow: hidden !important;
-    text-overflow: ellipsis;
+  width: 260px;
+  white-space: nowrap;
+  overflow: hidden !important;
+  text-overflow: ellipsis;
 }
 .amount-container {
   margin-top: 18px;
