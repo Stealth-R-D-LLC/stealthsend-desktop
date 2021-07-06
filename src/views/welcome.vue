7e75ca11d1ae2ee043ffb56397c5bfb5d857653d
Alex Dragun <alex.dragun@barrage.net>
2021-07-06 08:47:08 +0200

    XST-479 - change font size, reset validation on back button

--- src/views/welcome.vue
+++ src/views/welcome.vue
@@ -1958,18 +1958,18 @@
           <div v-if="recoveryStep === 4" class="step">
             <div>
               <h5>{{ isValidMnemonic ? 'Congratulations' : 'Error' }}</h5>
-              <p>
+              <h6>
                 {{
                   isValidMnemonic
                     ? 'Recovery Phrase successfully verified'
                     : 'Recovery Phrase is not valid'
                 }}
-              </p>
+              </h6>
             </div>
             <StButton v-if="isValidMnemonic" @click="recoveryStepNext"
               >Proceed</StButton
             >
-            <StButton v-else @click="recoveryStep = 0">Back</StButton>
+            <StButton v-else @click="goBack">Back</StButton>
           </div>
           <div class="step" v-if="recoveryStep === 5">
             <div>
@@ -2247,24 +2247,27 @@ export default {
           ],
         },
       });
-    const { form: recoveryForm, validateFields: validateRecoveryFields } =
-      useValidation({
-        account: {
-          $value: account,
-          $rules: [
-            {
-              rule: () => {
-                if (!account.value) {
-                  return 'Account name is required';
-                }
-                if (account.value.length > 50) {
-                  return 'Name too long';
-                }
-              },
+    const {
+      form: recoveryForm,
+      validateFields: validateRecoveryFields,
+      resetFields: resetRecoveryFields,
+    } = useValidation({
+      account: {
+        $value: account,
+        $rules: [
+          {
+            rule: () => {
+              if (!account.value) {
+                return 'Account name is required';
+              }
+              if (account.value.length > 50) {
+                return 'Name too long';
+              }
             },
-          ],
-        },
-      });
+          },
+        ],
+      },
+    });
 
     watchEffect(async () => {
       if (currentStep.value === 5) {
@@ -2539,6 +2542,11 @@ export default {
       }
     }
 
+    function goBack() {
+      recoveryStep.value = 0;
+      resetRecoveryFields();
+    }
+
     function handleAnimation(anim) {
       animation.value = anim;
     }
@@ -2607,6 +2615,7 @@ export default {
       createdMnemonic,
       createNewWallet,
       recoveryForm,
+      goBack,
 
       version,
 
