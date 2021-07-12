0317979c89f2b6f9069db7822d1eae8f1ab527b0
Matej Lazrevic <matej.lazarevic@barrage.net>
2021-07-12 12:06:29 +0200

    XST-607

--- src/background.js
+++ src/background.js
@@ -71,7 +71,6 @@ async function createWindow() {
   });
 }
 
-
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
   // On macOS it is common for applications and their menu bar
