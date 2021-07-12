f9471db4ecd6bf105eea7cc66979a6551705dd9a
dekadentno <matej.lazarevich@gmail.com>
2021-07-12 09:25:06 +0200

    XST-607

--- src/helpers/create-window.js
+++ src/helpers/create-window.js
@@ -65,20 +65,16 @@ module.exports = function createWindow(options = {}) {
         { role: 'reload' },
         { role: 'forceReload' },
         { role: 'toggleDevTools' },
-      ]
+      ],
     },
     {
       label: 'Window',
-      submenu: [
-        { role: 'close' }
-      ]
+      submenu: [{ role: 'close' }],
     },
   ];
 
-
-  const menu = Menu.buildFromTemplate(menuTemplate)
-  Menu.setApplicationMenu(menu)
-
+  const menu = Menu.buildFromTemplate(menuTemplate);
+  Menu.setApplicationMenu(menu);
 
   webFrame.setZoomFactor(1);
   webFrame.setVisualZoomLevelLimits(1, 1);
