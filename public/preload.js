5342b48484d799efed466e4d5fefa1221ff8ba07
Matej Lazrevic <matej.lazarevic@barrage.net>
2021-07-15 09:48:58 +0200

    XST-611 fix limit resize

--- public/preload.js
+++ public/preload.js
@@ -4,26 +4,9 @@ contextBridge.exposeInMainWorld(
         send: ( channel, ...args ) => {
             ipcRenderer.send( channel, ...args )
         },
-        // invoke: ( channel, ...args ) => {
-        //     return ipcRenderer.invoke( channel, ...args )
-        // },
-        // sendSync: ( channel, ...args ) => {
-        //     return ipcRenderer.sendSync( channel, ...args )
-        // },
         on: ( channel, listener ) => {
-            console.log('channel', channel);
-            console.log('listener: ', listener);
             ipcRenderer.on( channel, listener )
         },
-        // once: ( channel, listener ) => {
-        //     ipcRenderer.once( channel, listener )
-        // },
-        // removeListener: ( channel, listener ) => {
-        //     ipcRenderer.removeListener( channel, listener )
-        // },
-        // removeAllListeners: ( channel ) => {
-        //     ipcRenderer.removeAllListeners( channel )
-        // }
     }
 )
-// window.ipcRenderer = ipcRenderer
+window.ipcRenderer = ipcRenderer
