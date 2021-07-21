a08e50334da9c93b896661e64d27ecc2fdeaf0d2
Alex Dragun <alex.dragun@barrage.net>
2021-07-21 13:27:06 +0200

    XST-656 - Copy/Paste

--- src/background.js
+++ src/background.js
@@ -127,6 +127,19 @@ async function createWindow() {
       label: 'Window',
       submenu: [{ role: 'close' }],
     },
+    {
+      label: 'Edit',
+      submenu: [
+        { label: 'Cut', accelerator: 'CmdOrCtrl+X', selector: 'cut:' },
+        { label: 'Copy', accelerator: 'CmdOrCtrl+C', selector: 'copy:' },
+        { label: 'Paste', accelerator: 'CmdOrCtrl+V', selector: 'paste:' },
+        {
+          label: 'Select All',
+          accelerator: 'CmdOrCtrl+A',
+          selector: 'selectAll:',
+        },
+      ],
+    },
   ];
 
   const menu = Menu.buildFromTemplate(menuTemplate);
