3109cf408e7ef1a047c3c0a899916734b3334352
Marko Rajkovic <marko.rajkovic@barrage.net>
2021-07-27 19:37:22 +0200

    XST-719: Removed comma from displaying version of the app

--- src/views/welcome.vue
+++ src/views/welcome.vue
@@ -70,14 +70,14 @@
                     World’s first feeless,<br />private high performance<br />blockchain
                     protocol
                   </h3>
-                  <div class="app-version">StealthSend, v{{ version }}</div>
+                  <div class="app-version">StealthSend v{{ version }}</div>
                 </template>
                 <template v-if="isRecovery">
                   <h3 class="title">
                     World’s first feeless,<br />private high performance<br />blockchain
                     protocol
                   </h3>
-                  <div class="app-version">StealthSend, v{{ version }}</div>
+                  <div class="app-version">StealthSend v{{ version }}</div>
                 </template>
                 <template v-if="!isAccountFinished">
                   <div v-if="!isAccount && !isRecovery">
