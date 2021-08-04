3fdac075310bd07f801b932d7a07b042ce621c89
Alex Dragun <alex.dragun@barrage.net>
2021-08-04 12:17:17 +0200

    XST-707 - add/remove accounts from favorite, archive/active account delay

--- src/services/crypto.js
+++ src/services/crypto.js
@@ -248,10 +248,7 @@ const CryptoService = {
     let accounts = await db.getItem('accounts');
     if (accounts.length < 1) {
       console.error('Accounts do not exist');
-      return this.scanWallet();
-    } else if (accounts.filter((acc) => !acc.isArchived).length === 1) {
-      console.error('Cannot archive all accounts');
-      return this.scanWallet();
+      return;
     }
 
     const wantedIndex = accounts.findIndex(
@@ -269,8 +266,6 @@ const CryptoService = {
     accounts[wantedIndex].favouritePosition = null;
 
     await db.setItem('accounts', accounts);
-
-    return this.scanWallet();
   },
 
   async favouriteAccount(account) {
@@ -616,6 +611,7 @@ const CryptoService = {
         txs: txs, // all transactions,
         accounts: newAccounts,
       });
+      await db.setItem('accounts', newAccounts);
     });
   },
   nextToUse(freeAddresses) {
