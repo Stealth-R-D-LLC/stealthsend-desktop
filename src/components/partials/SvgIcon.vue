4866b06b30e908b5af622a67fa8e0d84e8591ba9
Andrea Mihaljevic <andrea.mihaljevic@barrage.net>
2021-08-10 08:45:09 +0200

    MenuBar SVGs to SvgIcon component

---
+++ src/components/partials/SvgIcon.vue
@@ -0,0 +1,27 @@
+<template>
+  <inline-svg :src="iconComponent" class="icon-svg"></inline-svg>
+</template>
+
+<script>
+import InlineSvg from 'vue-inline-svg';
+
+export default {
+  name: 'SvgIcon',
+  components: {
+    InlineSvg,
+  },
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    iconComponent() {
+      return require(`@/assets/icons/${this.name}.svg`);
+    },
+  },
+};
+</script>
+
+<style></style>
--- vue.config.js
+++ vue.config.js
@@ -1,66 +1,69 @@
-const path = require("path");
-const vueSrc = "./src";
+const path = require('path');
+const vueSrc = './src';
 // const GoogleFontsPlugin = require("@beyonk/google-fonts-webpack-plugin")
 module.exports = {
   runtimeCompiler: true,
   css: {
-    requireModuleExtension: true
+    requireModuleExtension: true,
   },
   configureWebpack: {
     resolve: {
       alias: {
-        "@": path.resolve(__dirname, vueSrc)
+        '@': path.resolve(__dirname, vueSrc),
       },
-      extensions: ['.js', '.vue', '.json']
+      extensions: ['.js', '.vue', '.json'],
     },
     plugins: [
-     /*  new GoogleFontsPlugin({
+      /*  new GoogleFontsPlugin({
           fonts: [
               { family: "Source Sans Pro", variants: [ "300", "400", "600", "700", "900" ] },
               { family: "Noto Sans", variants: [ "400", "700" ] }
           ]
           /* ...options
       })*/
-  ],
+    ],
   },
   pluginOptions: {
     electronBuilder: {
       builderOptions: {
-        "appId": "org.stealthsend.desktop",
-        "productName": "StealthSend",
-        "copyright": "Copyright © 2021 ${author}",
-        "afterSign": "scripts/notarize.js",
-        "dmg": {
-          "sign": false
+        appId: 'org.stealthsend.desktop',
+        productName: 'StealthSend',
+        copyright: 'Copyright © 2021 ${author}',
+        afterSign: 'scripts/notarize.js',
+        dmg: {
+          sign: false,
         },
-        "linux": {
-          "target": ["deb", "AppImage"],
-          "maintainer": "Stealth R&D LLC",
-          "icon": "src/assets/logo.png",
-          "description": "Stealth is the fastest cryptographically private digital currency. With blazing fast 5 second blocks, Junaeth is a game-changer and offers what state-of-the-art payment systems of the future require: a fast, feeless, private and scalable crypto.",
-          "category": "Development, Science, Utility"
+        linux: {
+          target: ['deb', 'AppImage'],
+          maintainer: 'Stealth R&D LLC',
+          icon: 'src/assets/logo.png',
+          description:
+            'Stealth is the fastest cryptographically private digital currency. With blazing fast 5 second blocks, Junaeth is a game-changer and offers what state-of-the-art payment systems of the future require: a fast, feeless, private and scalable crypto.',
+          category: 'Development, Science, Utility',
         },
-        "win": {
-          "target": ["nsis", "msi"],
-          "icon": "src/assets/logo.png"
+        win: {
+          target: ['nsis', 'msi'],
+          icon: 'src/assets/logo.png',
         },
-        "mac": {
-          "target": ['dmg'],
-          "icon": "src/assets/icon.icns",
-          "type": "distribution",
-          "entitlements": "build/entitlements.mac.plist",
-          "entitlementsInherit": "build/entitlements.mac.plist",
-          "extendInfo": {
-            "NSCameraUsageDescription": "This app requires camera access to record video.",
-            "NSMicrophoneUsageDescription": "This app requires microphone access to record audio.",
-            "NSAppTransportSecurity": {
-              "NSAllowsArbitraryLoads": true
-            }
+        mac: {
+          target: ['dmg'],
+          icon: 'src/assets/icon.icns',
+          type: 'distribution',
+          entitlements: 'build/entitlements.mac.plist',
+          entitlementsInherit: 'build/entitlements.mac.plist',
+          extendInfo: {
+            NSCameraUsageDescription:
+              'This app requires camera access to record video.',
+            NSMicrophoneUsageDescription:
+              'This app requires microphone access to record audio.',
+            NSAppTransportSecurity: {
+              NSAllowsArbitraryLoads: true,
+            },
           },
-        }
+        },
       },
       // nodeIntegration: true,
       // customFileProtocol: './'
-    }
+    },
   },
 };
