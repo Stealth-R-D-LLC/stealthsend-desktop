6e1d0f83117e2a83232daa94ced551f76cc1d3c9
kpetrovic <karlo.petrovic@barrage.net>
2021-08-23 11:48:18 +0200

    adding feeless for imported acc transactions

--- src/composables/useTransactionBuilderForImportedAccount.js
+++ src/composables/useTransactionBuilderForImportedAccount.js
@@ -1,14 +1,22 @@
 import useFeeEstimator from '@/composables/useFeeEstimator';
 import CryptoService from '@/services/crypto';
+import FeelessJS from '@/services/feeless';
 import { useMainStore } from '@/store';
-import * as bitcoin from '../../bitcoinjs-lib-feeless/src/index.js';
+import * as bitcoinFeeless from '../../bitcoinjs-lib-feeless/src/index.js';
+import * as bitcoin from 'bitcoinjs-lib';
 import { Buffer } from 'buffer';
 import { add, format, subtract, multiply } from 'mathjs';
 
 export default async function useTransactionBuilder(utxo, sendForm) {
   const mainStore = useMainStore();
 
-  const { fee } = useFeeEstimator(utxo.length);
+  let fee = 0;
+  if (!sendForm.isFeeless) {
+    const feeEstimator = useFeeEstimator(utxo.length);
+    fee = feeEstimator.fee;
+  }
+
+  console.log('TRANSACTION BUILDER: latest fee:', fee);
 
   const sumOf = (x = 0, y = 0) => {
     let sum = add(x, y);
@@ -35,11 +43,19 @@ export default async function useTransactionBuilder(utxo, sendForm) {
   async function buildTransaction() {
     // eslint-disable-next-line no-async-promise-executor
     return new Promise(async (resolve) => {
-      let rawTransaction = new bitcoin.TransactionBuilder(
-        CryptoService.network,
-        3000000
-      );
-
+      // eslint-disable-next-line no-async-promise-executor
+      let rawTransaction = null;
+      if (sendForm.isFeeless) {
+        rawTransaction = new bitcoinFeeless.TransactionBuilder(
+         CryptoService.network,
+         3000000
+        );
+      } else {
+        rawTransaction = new bitcoin.TransactionBuilder(
+         CryptoService.network,
+         3000000
+        );
+      }
       // add all outputs
       for await (let tx of utxo) {
         // get prevoutscript
@@ -57,10 +73,7 @@ export default async function useTransactionBuilder(utxo, sendForm) {
 
       let recipient = {
         address: sendForm.address,
-        amount: multiplyOf(
-          Number(sumOf(sendForm.amount, multiplyOf(fee, -1))),
-          1e6
-        ),
+        amount: Number(sumOf(sendForm.amount, fee * -1)) * 1e6,
       };
 
       let sumUtxo = utxo
@@ -83,6 +96,50 @@ export default async function useTransactionBuilder(utxo, sendForm) {
         rawTransaction.addOutput(change.address, change.amount);
       }
 
+      // create feework and feeless scriptPubkey and add output for feeless trx
+      if (fee === 0) {
+
+        rawTransaction.setVersion(4);
+        const bestBlock = await mainStore.rpc('getbestblock', []);
+
+        const txUnsignedHex = rawTransaction.buildIncomplete().toHex();
+        console.log('FEELESS txUnsignedHex: ', txUnsignedHex);
+        console.log('FEELESS height: ', bestBlock.height);
+        console.log('FEELESS size: ', bestBlock.size);
+        console.log('FEELESS hash: ', bestBlock.hash);
+        console.time('FEELESS create_feework_and_script_pubkey');
+
+        const feelessScriptPubkey = await FeelessJS.createFeeworkAndScriptPubkey(
+         txUnsignedHex,
+         bestBlock.height,
+         bestBlock.size,
+         bestBlock.hash
+        );
+
+        console.timeEnd('FEELESS create_feework_and_script_pubkey');
+
+        console.log(
+         'TRANSACTION BUILDER: feeless script sig key hex: ',
+         feelessScriptPubkey.toString('hex')
+        );
+        const testFeelessScriptPubkey =
+         await FeelessJS.testCreateFeeworkAndScriptPubkey(
+          txUnsignedHex,
+          bestBlock.height,
+          bestBlock.size,
+          bestBlock.hash,
+          feelessScriptPubkey.toString('hex')
+         );
+        console.log(
+         'FEELESS test results for script pubkey: ',
+         testFeelessScriptPubkey
+        );
+        rawTransaction.addOutput(Buffer.from(feelessScriptPubkey, 'hex'), 0);
+        console.log(
+         'TRANSACTION BUILDER: added output with zero amount and opcode OP_FEEWORK'
+        );
+      }
+
       const wallet = await CryptoService.getWalletFromDb();
       if (!wallet.password) {
         console.error('Invalid wallet');
@@ -94,16 +151,32 @@ export default async function useTransactionBuilder(utxo, sendForm) {
         wallet.password
       );
 
-      const keyPair = bitcoin.ECPair.fromWIF(
-        decryptedWIF,
-        CryptoService.network
-      );
-
-      for (let i = 0; i < utxo.length; i++) {
-        rawTransaction.sign(i, keyPair);
+      let keyPair = null;
+      if (sendForm.isFeeless) {
+        keyPair = bitcoinFeeless.ECPair.fromWIF(
+         decryptedWIF,
+         CryptoService.network
+        );
+      } else {
+        keyPair = bitcoin.ECPair.fromWIF(
+         decryptedWIF,
+         CryptoService.network
+        );
       }
 
+      for (let i = 0; i < utxo.length; i++) {
+
+        try {
+          rawTransaction.sign(i, keyPair);
+        } catch (e) {
+          console.dir(e);
+          throw new Error('TRANSACTION BUILDER: cannot sign tx: ', e);
+        }
+      }
+
+      console.dir(rawTransaction);
       const rawTransactionToHex = rawTransaction.build().toHex();
+      console.dir(rawTransactionToHex);
 
       const res = await mainStore.rpc('sendrawtransaction', [
         rawTransactionToHex,
