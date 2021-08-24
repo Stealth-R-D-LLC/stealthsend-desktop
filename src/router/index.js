da71ecb22c7328da5b8e03af3b4223514ac55ace
Matej Lazrevic <matej.lazarevic@barrage.net>
2021-08-24 13:54:05 +0200

    dynamic import components in router

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,29 @@
-const RpcError = () => import('@/components/connection/NoStealthConnection.vue');
+const RpcError = () =>
+  import('@/components/connection/NoStealthConnection.vue');
 const ArchivedAccounts = () => import('@/views/account/archived.vue');
 const AccountDetails = () => import('@/views/account/details.vue');
 const AddressBook = () => import('@/views/address-book.vue');
 const Dashboard = () => import('@/views/dashboard/dashboard.vue');
 const Lock = () => import('@/views/lock.vue');
-const SettingsAppNotifications = () => import('@/views/settings/app-notifications.vue');
+const SettingsAppNotifications = () =>
+  import('@/views/settings/app-notifications.vue');
 const SettingsAutoLock = () => import('@/views/settings/auto-lock.vue');
-const SettingsDeleteAppData = () => import('@/views/settings/delete-app-data.vue');
-const SettingsDesktopNotifications = () => import('@/views/settings/desktop-notifications.vue');
-const SettingsEmailNotifications = () => import('@/views/settings/email-notifications.vue');
+const SettingsDeleteAppData = () =>
+  import('@/views/settings/delete-app-data.vue');
+const SettingsDesktopNotifications = () =>
+  import('@/views/settings/desktop-notifications.vue');
+const SettingsEmailNotifications = () =>
+  import('@/views/settings/email-notifications.vue');
 const SettingsFaq = () => import('@/views/settings/faq.vue');
-const SettingsGeneralInformation = () => import('@/views/settings/general-information.vue');
+const SettingsGeneralInformation = () =>
+  import('@/views/settings/general-information.vue');
 const SettingsMyTickets = () => import('@/views/settings/my-tickets.vue');
 const SettingsNewTicket = () => import('@/views/settings/new-ticket.vue');
 const SettingsPassword = () => import('@/views/settings/password.vue');
-const SettingsPrivacyPolicy = () => import('@/views/settings/privacy-policy.vue');
-const SettingsRecoveryPhrase = () => import('@/views/settings/recovery-phrase.vue');
+const SettingsPrivacyPolicy = () =>
+  import('@/views/settings/privacy-policy.vue');
+const SettingsRecoveryPhrase = () =>
+  import('@/views/settings/recovery-phrase.vue');
 const SettingsReportABug = () => import('@/views/settings/report-a-bug.vue');
 const Settings = () => import('@/views/settings/settings.vue');
 const SettingsTermsOfUse = () => import('@/views/settings/terms-of-use.vue');
