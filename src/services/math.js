c67bbddf92cc3b1397de1577adf8596a9728fa23
Matej Lazarevic <matej.lazarevic@barrage.net>
2021-12-10 12:03:24 +0100

    remove devtools and redundant logs

--- src/services/math.js
+++ src/services/math.js
@@ -7,19 +7,12 @@ const math = create(all, {
 
 const MathService = {
   add(x, y) {
-    console.log(`MATH add: ${x} + ${y} = ${math.add(x, y).toFixed(6)}`);
     return math.add(x, y).toFixed(6);
   },
   subtract(x, y) {
-    console.log(
-      `MATH subtract: ${x} - ${y} = ${math.subtract(x, y).toFixed(6)}`
-    );
     return math.subtract(x, y).toFixed(6);
   },
   multiply(x, y) {
-    console.log(
-      `MATH multiply: ${x} x ${y} = ${math.multiply(x, y).toFixed(6)}`
-    );
     return math.multiply(x, y).toFixed(6);
   },
 };
