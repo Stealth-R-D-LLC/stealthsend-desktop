6b6b08e8404a00b6b2721f581895204b8e1a722c
Matej Lazarevic <matej.lazarevic@barrage.net>
2021-12-23 09:31:55 +0100

    XST-993 no gap on address discovery

--- src/background.js
+++ src/background.js
@@ -163,10 +163,10 @@ async function createWindow() {
         },
       ],
     },
-    // {
-    //   label: 'View',
-    //   submenu: [{ role: 'toggleDevTools' }],
-    // },
+    {
+      label: 'View',
+      submenu: [{ role: 'toggleDevTools' }],
+    },
     {
       label: 'Window',
       submenu: [{ role: 'minimize' }],
@@ -347,7 +347,6 @@ app.on('ready', async () => {
         "style-src 'unsafe-inline' 'self' https://fonts.googleapis.com app:;",
         "style-src-elem 'unsafe-inline' https://fonts.googleapis.com 'self' https://fonts.googleapis.com app:;",
         "font-src https://fonts.gstatic.com 'self';",
-        // "default-src 'self' https://api-mainnet.stealthmonitor.xyz https://api.stealth.org 172.20.10.112:8080 app:;",
         "connect-src 'self' https://api-mainnet.stealthmonitor.xyz https://api.stealth.org 172.20.10.169:8080 app:;",
         "script-src 'unsafe-inline' 'unsafe-eval' 'self' app: blob:;",
         "script-src-elem 'unsafe-inline' 'self' app:;",
