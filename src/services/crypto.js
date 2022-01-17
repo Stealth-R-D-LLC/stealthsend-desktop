71d61dc677ffe825168e2083e24937b244d50d6a
Matej Lazarevic <matej.lazarevic@barrage.net>
2022-01-17 14:23:04 +0100

    XST-997 fix searching for change address

--- src/services/crypto.js
+++ src/services/crypto.js
@@ -463,7 +463,11 @@ const CryptoService = {
   async addressDiscovery(n = 0, change = 0, lastUsedAddress = 0) {
     // used for searching for the change address or the deposit address
     const mainStore = useMainStore();
-    lastUsedAddress = await this.getLastUsedAddress(`${n}'/0/0`); // previously set in db
+    if (change === 0) {
+      // in case of searching for the deposit address, we can use the last used address
+      // in case of searching for the change address, we'll skip this and search for it from the beginning 
+      lastUsedAddress = await this.getLastUsedAddress(`${n}'/0/0`); // previously set in db
+    } 
 
     for (let i = lastUsedAddress; i < Infinity; i++) {
       // derive the first account's node (index = 0)
