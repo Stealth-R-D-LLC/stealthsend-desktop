2e930be323acc949defcf4feb481867ea2f15d88
kpetrovic <karlo.petrovic@barrage.net>
2024-12-10 10:57:27 +0100

    Fix ci

--- scripts/notarize.js
+++ scripts/notarize.js
@@ -10,10 +10,10 @@ exports.default = async function notarizing(context) {
   const appName = packager.appInfo.productFilename;
 
   return await notarize({
-    appBundleId: process.env.NOTARIZE_APP_BUNDLE_ID,
+    //appBundleId: process.env.NOTARIZE_APP_BUNDLE_ID,
     appPath: `${appOutDir}/${appName}.app`,
     appleId: process.env.NOTARIZE_APPLE_ID,
     appleIdPassword: process.env.NOTARIZE_APPLE_ID_PASSWORD,
-    ascProvider: process.env.NOTARIZE_ASC_PROVIDER,
+    teamId: process.env.NOTARIZE_ASC_PROVIDER,
   });
 };
