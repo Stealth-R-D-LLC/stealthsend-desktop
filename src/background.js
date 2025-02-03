ed375c3db098cf30d3b91818c0934f20e31c36eb
kpetrovic <karlo.petrovic@barrage.net>
2025-02-03 15:32:46 +0100

    Fix CSP

--- src/background.js
+++ src/background.js
@@ -328,7 +328,7 @@ app.on('ready', async () => {
         "style-src-elem 'unsafe-inline' https://fonts.googleapis.com 'self' https://fonts.googleapis.com app:;",
         "font-src https://fonts.gstatic.com 'self';",
         // "default-src 'self' https://api-mainnet.stealthmonitor.xyz https://api.stealth.org 172.20.10.112:8080 app:;",
-        "connect-src 'self' https://*.sentry.io https://api-gateway-mainnet.stealthmonitor.xyz https://api.stealth.org 172.20.10.169:8080 app:;",
+        "connect-src 'self' https://api-gateway-mainnet.stealthmonitor.xyz https://api.stealth.org 172.20.10.169:8080 app:;",
         "script-src 'unsafe-inline' 'unsafe-eval' 'self' app: blob:;",
         "script-src-elem 'unsafe-inline' 'self' app:;",
         "img-src 'self' app: data:;",
